cf6146e3e8ad37b511a820ed30a1099a1e950ecf
David Montano <cuatro04sr@gmail.com>
2022-02-05 00:41:50 -0500

    feat: add pages flows

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,24 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using RazorPagesUser.Models;
 
 namespace RPCAuthenticationSystem.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly RazorPagesUsers.Data.RazorPagesUsersContext _context;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, RazorPagesUsers.Data.RazorPagesUsersContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [BindProperty]
-        public User User { get; set; }
+        public string Username { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            return Page();
+        }
 
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = from m in _context.User select m;
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var dbEntry = _context.User.FirstOrDefault(acc => acc.Username == Username);
+            return RedirectToPage("Users/Index");
         }
     }
 }
